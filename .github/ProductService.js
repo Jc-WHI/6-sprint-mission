5b557188bf09494b9f8880bccc6de32ae7b0b76a
조치호 <gh30212@naver.com>
2025-10-15 14:04:42 +0900

    modified:   .github/ProductService.js     수정

--- .github/ProductService.js
+++ .github/ProductService.js
@@ -19,28 +19,34 @@ export class Product{
 export class ElectricProduct extends Product{
     constructor(name,description,price,tags,images,favoriteCount=0,manufacturer){
 
-                this.name = name;
-        this.description = description;
-        this.price = price;
-        this.tags = tags;
-        this.images = images;
-        this.favoriteCount = favoriteCount;
-        this.manufacturer = manufacturer;
+    super(name, description, price, tags, images, favoriteCount);
+    this.manufacturer = manufacturer;
     }
 
 
 }
 
 export async function getProductList(page=1,pageSize=10,orderBy="recent",keyword){
-    const params = new URLSearchParams({page:page,pageSize:pageSize,orderBy:orderBy});
-    const baseurl =`https://panda-market-api-crud.vercel.app/docs/#/Product/ListProducts${params}`;
-        let result=await axios.get(baseurl,{
-        method:"GET"
-    }).then((response)=>{console.log('get response is sucessfully done')})
-    .catch((error)=>{console.log(error)})
+    const baseurl =`https://panda-market-api-crud.vercel.app/products?`;
+    try{    
+    let result=await axios.get(baseurl,{
+        params:{
+            page:page,
+            pageSize:pageSize,
+            orderBy:orderBy,
+            keyword:keyword
+        }
+   
+    })
+    console.log('GET request is sucessfully done');
+    return result.data;
+}catch(error){
+    console.error('GET request is failed',error.message);
+}
      
     
 
 }
 
-getProductList(1,10,recent,제품);
+let result = await getProductList(1,10,"recent","");
+console.log(result);
