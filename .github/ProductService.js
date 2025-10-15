41abd467fb2419fc941654b63db82bc1c27afe0c
조치호 <gh30212@naver.com>
2025-10-15 17:59:10 +0900

    "ProductFinished"

--- .github/ProductService.js
+++ .github/ProductService.js
@@ -27,6 +27,7 @@ export class ElectricProduct extends Product{
 }
 
 export async function getProductList(page=1,pageSize=10,orderBy="recent",keyword){
+
     const baseurl =`https://panda-market-api-crud.vercel.app/products?`;
     try{    
     let result=await axios.get(baseurl,{
@@ -39,7 +40,19 @@ export async function getProductList(page=1,pageSize=10,orderBy="recent",keyword
    
     })
     console.log('GET request is sucessfully done');
-    return result.data;
+    const products = result.data.list.map(element =>{
+        if(element["tags"].includes("전자제품")){
+            
+             return new ElectricProduct(element.name,element.description,element.price,element.tags,element.images);
+
+        }else{
+            return new Product(element.name,element.description,element.price,element.tags,element.images);
+        }
+        
+        
+        
+    });
+    return products;
 }catch(error){
     console.error('GET request is failed',error.message);
 }
@@ -48,5 +61,49 @@ export async function getProductList(page=1,pageSize=10,orderBy="recent",keyword
 
 }
 
-let result = await getProductList(1,10,"recent","");
-console.log(result);
+
+export async function createProduct(name, description, price, tags, images){
+    const baseurl =`https://panda-market-api-crud.vercel.app/products`;
+    try{
+        let request = await axios.post(baseurl,
+            {
+                name:name,
+                description:description,
+                price:price,
+                tags:tags,
+                images:images
+            }
+        )
+        console.log("Post is suceed",request.data);
+        return request.data;
+    }catch(error){
+        console.log("post failed:",error.response.data);
+
+    }
+
+    
+}
+
+export async function patchProduct(productid,userData){
+    const baseurl =`https://panda-market-api-crud.vercel.app/products/${productid}`;
+    try{
+        let request = await axios.patch(baseurl,userData)
+            return request.data
+    }catch(error){
+        console.log("post failed:",error.response.data);
+
+    }
+
+}
+
+
+export async function deleteProduct(productid){
+    const baseurl =`https://panda-market-api-crud.vercel.app/products/${productid}`;
+    try{
+        let request = await axios.delete(baseurl)
+        return request.data;
+    }catch(error){
+        console.log("delete failed",error.response.data);
+    }
+}
+
